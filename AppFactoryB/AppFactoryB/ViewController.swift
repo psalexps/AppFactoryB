3da12ef9ef7e530774a258460ff5ce5612ee0dac
alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2020-01-31 21:56:54 +0100

    creando cronometro

--- AppFactoryB/AppFactoryB/ViewController.swift
+++ AppFactoryB/AppFactoryB/ViewController.swift
@@ -18,9 +18,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var finishButton: UIButton!
     
     var timer: Timer?
+    var time: Double = 0
     
     var seconds :Int = 0
     var miliSeconds :Int = 0
+    
+    var startTime: Double = 0
+    
+    var ms: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,19 +34,48 @@ class ViewController: UIViewController {
 
     @IBAction func start(_ sender: Any) {
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        /*let interval :Double = 0.02
+        
+        timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)*/
+        
+        startTime = Date().timeIntervalSinceReferenceDate
+        timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                     target: self,
+                                     selector: #selector(fireTimer),
+                                     userInfo: nil,
+                                     repeats: true)
     }
     
     @objc func fireTimer() {
 
+        /*ms += Int(timer!.timeInterval * 10)
+        
         miliSeconds += Int(timer!.timeInterval * 10)
         
-        if(miliSeconds >= 10) {
+        /*if(ms >= 10) {
+            
             seconds += 1
+        }*/
+        
+        timeLabel.text = "\(miliSeconds)"*/
+        
+        //Total time since timer started, in seconds
+        time = Date().timeIntervalSinceReferenceDate - startTime
+
+        //Convert the time to a string with 2 decimal places
+        
+        //miliSeconds = String(format: "%.2f", time)
+        
+        miliSeconds += Int(time * 100)
+        
+        if (miliSeconds >= 100) {
+            
             miliSeconds = 0
+            seconds += 1
         }
         
         timeLabel.text = "\(seconds):\(miliSeconds)"
+        
     }
     
 }
